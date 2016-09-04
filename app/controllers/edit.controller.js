cd303361a401433eda87317fff1df0ec8433c84a
Rexford <seanmavley@gmail.com>
2016-09-04 22:25:46 +0000

    3-way binding firebase

--- app/controllers/edit.controller.js
+++ app/controllers/edit.controller.js
@@ -1,10 +1,13 @@
 angular.module('madeWithFirebase')
 
-.controller('EditController', ['$scope', '$state', '$firebaseObject', 'DatabaseRef', 'Auth', 'currentAuth',
-  function($scope, $state, $firebaseObject, $firebaseArray, DatabaseRef, Auth, currentAuth) {
-    
+.controller('EditController', ['$scope', '$firebaseObject', 'DatabaseRef', '$stateParams',
+  function($scope, $firebaseObject, DatabaseRef, $stateParams) {
+    console.log('Controller loaded');
+
     var fire = $firebaseObject(DatabaseRef.child('fire').child($stateParams.fireId));
 
+    $scope.loading = true;
+    
     var categoryObject = $firebaseObject(DatabaseRef.child('categories'));
     categoryObject.$loaded()
       .then(function(data) {
@@ -13,8 +16,11 @@ angular.module('madeWithFirebase')
         toastr.error(error.message, 'Couldnt not load categories');
       });
 
+    var now = new Date().getTime();
+
     fire.$bindTo($scope, "formData")
       .then(function() {
+        $scope.loading = false;
         $scope.formData.updatedAt = now;
         fire.update($scope.formData)
           .then(function(data) {
