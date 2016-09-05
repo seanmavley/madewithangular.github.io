4f2efa50643e9db267c624e3c0ce3d96c2e4f156
Rexford <seanmavley@gmail.com>
2016-09-05 20:00:49 +0000

    improvements

--- app/controllers/new.controller.js
+++ app/controllers/new.controller.js
@@ -2,6 +2,8 @@ angular.module('madeWithFirebase')
 
 .controller('CreateController', ['$scope', '$state', '$firebaseObject', '$firebaseArray', 'DatabaseRef', 'Auth', 'currentAuth',
   function($scope, $state, $firebaseObject, $firebaseArray, DatabaseRef, Auth, currentAuth) {
+    $scope.sending = false; 
+
     var list = $firebaseArray(DatabaseRef.child('fire'));
 
     var categoryObject = $firebaseObject(DatabaseRef.child('categories'));
@@ -19,6 +21,8 @@ angular.module('madeWithFirebase')
           'It means a lot!', 'Incomplete form');
 
       } else {
+        toastr.info('Saving begins', 'Sending your submission to the Firebase. Hold on!');
+        $scope.sending = true;
         var now = new Date().getTime();
 
         $scope.formData.uid = currentAuth.uid;
@@ -26,14 +30,15 @@ angular.module('madeWithFirebase')
         $scope.formData.createdAt = now;
 
         // console.log($scope.formData);
-        
+
         list.$add($scope.formData)
           .then(function(saved) {
-            console.log('Saving happened', saved);
+            toastr.clear();
+            // console.log('Saving happened', saved);
             toastr.success('Saving happened');
             $state.go('fire', { fireId: saved.key });
           }, function(error) {
-            console.log(error.reason, error.message);
+            // console.log(error.reason, error.message);
             toastr.error(error.reason, error.message)
           })
       }
