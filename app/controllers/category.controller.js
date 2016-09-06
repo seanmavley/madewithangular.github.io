25e0eaa83a4079099ee9c9f0e3c56b991d5ce561
Rexford <seanmavley@gmail.com>
2016-09-06 17:17:19 +0000

    added category

--- app/controllers/category.controller.js
+++ app/controllers/category.controller.js
@@ -1,4 +1,17 @@
 angular.module('madeWithFirebase')
-  .controller('CategoryController', ['$scope', function($scope){
-    console.log('Category Controller loaded');
-  }])
+  .controller('CategoryController', ['$scope', 'DatabaseRef', '$firebaseArray', '$stateParams',
+    function($scope, DatabaseRef, $firebaseArray, $stateParams) {
+      console.log('Category Controller loaded');
+      var query = DatabaseRef.child('fire').orderByChild('category').equalTo($stateParams.categoryId);
+      var list = $firebaseArray(query);
+
+      list.$loaded()
+        .then(function(data) {
+          console.log(data);
+          $scope.list = data
+        })
+        .catch(function(error) {
+          toastr.error(error.message);
+        })
+    }
+  ])
